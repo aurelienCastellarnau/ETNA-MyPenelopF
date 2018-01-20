3f36d102250e748e49ba5e15da12fee02fc535e3
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-01-20 14:00:39 +0100

    Ajout de deux classes graphique: BaseFrame étend JFrame, elle permet d'instancier une fenêtre. Une surcharge permet d'instancier une fenêtre affichant des Contacts en prenant un ArrayList<Contact> en paramètre. FileSystemManager permet de centraliser les méthodes liées aux accès R/W sur notre fileSytem. PenelopDevLogger usage: port salut, c'est écris dessus...

---
+++ MyPenelopF/src/main/java/utils/FileSystemManager.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+import classes.Contact;
+
+public class FileSystemManager {
+
+	String project_path = System.getProperty("user.dir");
+	
+	private FileSystemManager() {}
+	private static class SingletonHolder
+	{
+		private final static FileSystemManager instance = new FileSystemManager();
+	}
+	public static FileSystemManager get() {
+		return SingletonHolder.instance;
+	}
+	
+	public void writeContacts(ArrayList<Contact> users) {
+		Gson gson = new Gson();
+    	String json = gson.toJson(users);
+		try {
+			FileWriter fileWriter = new FileWriter(this.project_path + "/users.json");
+    		fileWriter.write(json);
+            fileWriter.close();
+		} catch (IOException e) {
+			System.out.println("Exception trhowed in writeContacts(): " + e.getMessage());
+		}
+	}
+}
---
+++ MyPenelopF/src/main/java/utils/PenelopDevLogger.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.util.ArrayList;
+
+import classes.Contact;
+
+public class PenelopDevLogger {
+
+	private PenelopDevLogger() {}
+	private static class SingletonHolder
+	{
+		private final static PenelopDevLogger instance = new PenelopDevLogger();
+	}
+	public static PenelopDevLogger get() {
+		return SingletonHolder.instance;
+	}
+	
+	public void contacts(ArrayList<Contact> contacts) {
+		for (Contact c: contacts) {
+        	System.out.println("User n°" + c.getId() + " added to users.");
+        	System.out.println("Email: " + c.getEmail());
+        	System.out.println("Surname: " + c.getSurname());
+        	System.out.println("Name: " + c.getName());
+        }
+	}
+}
