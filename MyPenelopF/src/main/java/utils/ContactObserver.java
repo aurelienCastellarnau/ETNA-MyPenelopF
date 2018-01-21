0f76e4d2ee5fe041218040ebb3038496e3b5b732
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-01-21 17:58:04 +0100

    simplification du pattern observer, on fait le choix d'implémenter des méthodes vides dans certaines classes pour éviter de démultiplier les fichiers.

---
+++ MyPenelopF/src/main/java/utils/ContactObserver.java
@@ -0,0 +1,13 @@
+package utils;
+
+import classes.Contact;
+
+public interface ContactObserver {
+	void addContactListener(ContactListener listener);
+	void removeContactListener(ContactListener listener);
+	void triggerContactChange();
+	void triggerShowUpdate(Contact contact);
+	void triggerCreateContact(Contact contact);
+	void triggerUpdateContact(Contact contact);
+	void triggerDeleteContact(Contact contact);
+}
