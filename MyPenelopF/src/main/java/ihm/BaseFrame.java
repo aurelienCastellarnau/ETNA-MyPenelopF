54780c898799464c36eac50266ef1f6acc35228b
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-01-21 02:21:05 +0100

    Ajout de la logique de refresh du ContactPanel. La solution n'est pas très propre, je recrée complètement la vue... dommage, un simple refresh des données serait préférable. Suppression d'un fichier class

--- MyPenelopF/src/main/java/ihm/BaseFrame.java
+++ MyPenelopF/src/main/java/ihm/BaseFrame.java
@@ -13,6 +13,12 @@ import javax.swing.JPanel;
 import classes.Contact;
 import controllers.ContactController;
 
+/**
+ * 
+ * @author aurelien
+ * This classe allow us to declare base views for each entities
+ * in few time we will split those constructors in several classes
+ */
 public class BaseFrame extends JFrame {
 	/**
 	 * JFrame implementation requirement
@@ -21,6 +27,7 @@ public class BaseFrame extends JFrame {
 	FormBuilder _fb = new FormBuilder();
 	ContactPanel contactPanel;
 	CreateContact createContact;
+	JPanel buttonPane;
 	CardLayout cl = new CardLayout();
 
 	public BaseFrame() {
@@ -34,33 +41,54 @@ public class BaseFrame extends JFrame {
         frame.setVisible(true);
 	}
 	
+	/**
+	 * 
+	 * @param cCtrl
+	 * @param users
+	 * BaseFrame for Contact Crud
+	 * Display result of getContacts in ContactPanel
+	 * Allow to create and write a new Contact list in CreateContact
+	 * buttonPane stock an instance of FormBuilder.getNavPanel
+	 * wich can be reuse with another CardLayout/JPanel display
+	 */
 	public BaseFrame(ContactController cCtrl, ArrayList<Contact> users) {
         JFrame frame = new JFrame("Users");
-		GridLayout gl = this.getGridLayout(2, 3, 5);
+		GridLayout gl = new GridLayout(2, 3, 5, 5);
         frame.setTitle("Users: ");
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(250, 250);
-        this.contactPanel = new ContactPanel(new JPanel(), cl, users);
+        this.contactPanel = new ContactPanel(new JPanel(), this.cl, users);
         this.createContact = new CreateContact(new JPanel());
         this.createContact.addCreateContactListener(cCtrl);
-        JPanel boutonPane = this._fb.getNavPanel(this.contactPanel.getCard(), this.contactPanel.getPan());
+        this.buttonPane = this._fb.getNavPanel(this.contactPanel.getCard(), this.contactPanel.getPan());
         this.setSize(800, 800);
         this.setLayout(gl);
-        this.getContentPane().add(boutonPane);
+        this.getContentPane().add(this.buttonPane);
         this.getContentPane().add(this.contactPanel.getPan());
         this.getContentPane().add(this.createContact.getPan());
         this.setVisible(true);
 	}
 	
-	private GridLayout getGridLayout(int cols, int rows, int gap) {
-		GridLayout gl = new GridLayout();
-		gl.setColumns(cols);
-		gl.setRows(rows);
-		gl.setHgap(gap);
-		gl.setVgap(gap);
-		return gl;
+	/**
+	 * 
+	 * @param contacts
+	 * recreate the content of the ContactPanel view
+	 * remove and add createContact to preserve order.
+	 * See for a non-destroying solution...
+	 */
+	public void refreshContactPanel(ArrayList<Contact> contacts) {
+		this.getContentPane().remove(this.buttonPane);
+		this.getContentPane().remove(this.contactPanel.getPan());
+		this.getContentPane().remove(this.createContact.getPan());
+		this.cl = new CardLayout();
+		this.contactPanel = new ContactPanel(new JPanel(), this.cl, contacts);
+        this.buttonPane = this._fb.getNavPanel(this.contactPanel.getCard(), this.contactPanel.getPan());
+        this.getContentPane().add(this.buttonPane);
+        this.getContentPane().add(this.contactPanel.getPan());
+		this.getContentPane().add(this.createContact.getPan());
+        this.setVisible(true);
 	}
 }
 
