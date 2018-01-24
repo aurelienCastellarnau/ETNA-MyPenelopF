20af16a675c831869bb4d9137e08d99d5e714c46
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-01-24 23:20:29 +0100

    Call sur ProjectController et Ajout du getter dans la DAOFactory

--- MyPenelopF/src/main/java/DAO/DAOFactory.java
+++ MyPenelopF/src/main/java/DAO/DAOFactory.java
@@ -2,6 +2,7 @@ package DAO;
 
 import DataInterface.DataInterface;
 import classes.Contact;
+import classes.Project;
 
 public class DAOFactory {
 	  
@@ -9,5 +10,11 @@ public class DAOFactory {
 		  // ContactDAO must implement ContactDAOReceipe
 		  ContactDAOReceipe cDAO = new ContactDAO(di);
 		  return (ContactDAO)cDAO;
+	  }
+	  
+	  public static DAO<Project> getProjectDAO(DataInterface di) {
+		  // ProjectDAO must implement ProjectDAOReceipe
+		  ProjectDAOReceipe pDAO = new ProjectDAO(di);
+		  return (ProjectDAO)pDAO;
 	  }
 }
--- MyPenelopF/src/main/java/controllers/AppController.java
+++ MyPenelopF/src/main/java/controllers/AppController.java
@@ -4,22 +4,27 @@ import java.util.HashMap;
 
 import DAO.ContactDAO;
 import DAO.DAOFactory;
+import DAO.ProjectDAO;
 import DataInterface.DataInterface;
 import ihm.BaseFrame;
+import classes.Project;
 
 public class AppController implements PenelopeController {
 	private HashMap<String, PenelopeController> ctrls = new HashMap<String, PenelopeController>();
 	private ContactDAO cDAO = null;
+	private ProjectDAO pDAO = null;
 	// protected GroupDAO gDAO = (GroupDAO) DAOFactory.getGroupDAO(FileSystemManager.get());
 	
 	private BaseFrame Dashboard = null;
 	
 	public AppController(DataInterface di) {
 		this.cDAO = (ContactDAO)DAOFactory.getContactDAO(di);
+		this.pDAO = (ProjectDAO)DAOFactory.getProjectDAO(di);
 	}
 	public void init() {
     	//App.ctrls.put("group", new GroupController(this.gDAO));
     	this.ctrls.put("contact", new ContactController(this.cDAO));
+    	this.ctrls.put("project", new ProjectController(this.pDAO));
     	// App.ctrls.put("xxx", new xxxController(this.xDAO));
     	for (String key: this.ctrls.keySet())
     		ctrls.get(key).init();
